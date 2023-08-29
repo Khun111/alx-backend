263161013993b19b88cdc89a9bb0d28b3bdbf022
Oluwatobi <amure387@gmail.com>
2023-08-29 14:08:05 +0000

    feat: completed task 4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+'''Basic flask app'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''Config class for babel settings'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@babel.localeselector
+def get_locale():
+    '''Function for setting locale'''
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home():
+    '''Index page view'''
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
