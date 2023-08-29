33a5a99be57de373f70592e06dcc2a5e46f1ff31
Oluwatobi <amure387@gmail.com>
2023-08-29 13:59:07 +0000

    fix: import request and edited lang request logic

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-
 '''Basic flask app'''
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
@@ -17,7 +16,7 @@ class Config:
 @babel.localeselector
 def get_locale():
     '''Function for setting locale'''
-    return request.accept_languages.bestmatch(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 app.config.from_object(Config)
