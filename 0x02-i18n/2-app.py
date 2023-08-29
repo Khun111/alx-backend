e722621e38e4a537966595e891ee88c47df6fee7
Oluwatobi <amure387@gmail.com>
2023-08-29 09:56:47 +0000

    feat: completed task 0,1, 2 and pushed to use sandbox

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.7
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    '''Config class for babel settings'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+@babel.localeselector
+def get_locale():
+    '''Function for setting locale'''
+    return request.accept_languages.bestmatch(app.config["LANGUAGES"])
+
+
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    '''Index page view'''
+    return render_template('2-index.html')
+
+if __name__ == '__main__':
+    app.run()
